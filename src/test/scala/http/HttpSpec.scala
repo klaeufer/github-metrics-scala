125ba6d7006e9fb7202e03618db20903d4a79e6a
Konstantin Läufer <laufer@cs.luc.edu>
2014-12-14 18:07:17 -0600

    stubbed out the test examples to match the API doc

--- src/test/scala/http/HttpSpec.scala
+++ src/test/scala/http/HttpSpec.scala
@@ -7,41 +7,43 @@ class HttpSpec extends Specification {
 
   val serviceRoot = "http://private-14b8e-clickcounter.apiary-mock.com/"
 
-  "The click counter service" should {
-    "return the correct value" in {
-      val response = Http(url(serviceRoot + "counters/123/value") OK as.String)
-      response().toInt === 3
+  "The click counter service, with respect to its collection of counters," should {
+
+    "return the newly created counter" in {
+      todo
+    }
+
+    "retrieve an existing counter" in {
+      todo
+    }
+
+    "delete an existing counter" in {
+      todo
     }
   }
 
-  //def get(resource: String) = {}
+  "The click counter service, with respect to a specific counter," should {
 
-  // create a counter
+    "increment the counter" in {
+      todo
+    }
 
+    "refuse to decrement the counter" in {
+      todo
+    }
 
-  // retrieve a counter
+    "reset the counter" in {
+      todo
+    }
 
+    "return the counter value" in {
+      val response = Http(url(serviceRoot + "counters/123/value") OK as.String)
+      response().toInt === 3
+    }
 
-  // delete a counter
+    "retrieve a counter value stream" in {
+      todo
+    }
 
-
-  // increment a counter
-
-
-
-  // decrement a counter
-
-
-
-  // reset a counter
-
-
-
-  // retrieve a counter value
-
-
-  // retrieve a counter value stream
-
-
-
+  }
 }
